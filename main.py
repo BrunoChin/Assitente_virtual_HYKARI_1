24c960c41f7b5ddf4eab4c05490164bfa897b83f
Bruno Henrique Silva de Oliveira <47747909+BrunoChin@users.noreply.github.com>
2023-04-30 08:59:40 -0300

    adição do comando de horas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from vosk import Model, KaldiRecognizer
 import pyaudio
 import json
 import pyttsx3
-
+import core
 
 #Sinteze de voz
 engine = pyttsx3.init()
@@ -18,18 +18,22 @@ model = Model("model_pt-br")
 recoginizer = KaldiRecognizer(model, 16000)
 
 cap     = pyaudio.PyAudio()
-stream  = cap.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream  = cap.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
 stream.start_stream()
 
 while True:
-    data = stream.read(4096)
+    data = stream.read(2048)
     if len(data) == 0:
        break
     if recoginizer.AcceptWaveform(data):
         result = recoginizer.Result()
         result = json.loads(result)
         
-        if result is not None:
+        if result['text'] is not None:
             text = result['text']
+        
             print(text)
+            
+            if text == 'que horas são' or text == 'me diga as horas':
+                speak(core.SystemInfo.get_time())
         
